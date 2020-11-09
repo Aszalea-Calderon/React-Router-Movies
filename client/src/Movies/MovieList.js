286c0cc149aa2180e89218e8acc0ff587136e452
Aszalea Calderon <aszalea_calderon@yahoo.com>
2020-11-09 15:48:30 -0800

    adding things and stuff

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,27 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
 
 export default function MovieList(props) {
+  //nav for browser
+  //const history = useHistory();
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
+      {props.movies.map((movie) => (
+        // <div onClick={() => history.push(movie.id)}>
         <MovieDetails key={movie.id} movie={movie} />
+        // </div>
       ))}
     </div>
   );
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  const { title, director, metascore, id } = props.movie;
 
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
+    <Link to={`/movies/${id}`}>
+      <div className="movie-card">
+        {/*movieID*/}
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
       </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
+    </Link>
   );
 }
